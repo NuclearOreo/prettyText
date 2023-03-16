7bec58a2f51e7766c30c8fcd47e61a8916537420
Ussama Azam <nuclearoreo@gmail.com>
2023-03-15 23:16:39 -0400

    Pretty ascii text

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,27 @@ mod parser;
 use clap::Parser;
 use parser::Args;
 
+//https://texteditor.com/ascii-art/
 fn main() {
     let args = Args::parse();
+    let output = b"".to_vec();
 
-    println!("{:?}", args.medium);
+    let test = r#"
+▄▄▄▄▄
+█░▄▄▀
+█░▀▀░
+█░██░
+▀▀▀▀▀"#;
+
+    let bytes = test.as_bytes();
+
+    let mut vec = bytes.to_vec();
+    vec.append(&mut bytes.to_vec());
+
+    match String::from_utf8(vec) {
+        Ok(result) => {
+            println!("{}", result);
+        }
+        Err(_) => panic!("Failed to output Art"),
+    };
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,13 +1,18 @@
 use clap::Parser;
 
+/// Pretty Text, CLI to convert your text or photo into ASCII Art
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    /// Number of times to greet
+    /// Convert from text or photo, [options: text, photo]
     #[arg(short, long, default_value = "text")]
     pub medium: String,
 
-    /// Name of the person to greet
+    /// Input text you want convert into ASCII
     #[arg(short, long)]
     pub text: String,
+
+    /// Font style of text
+    #[arg(short, long, default_value = "Meh")]
+    pub font: String,
 }

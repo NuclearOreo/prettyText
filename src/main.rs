579e11842c23562613a4329e38d73feac6eda949
Ussama Azam <nuclearoreo@gmail.com>
2023-03-17 21:40:42 -0400

    Basic setup for reading in image

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 mod parser;
 
 use clap::Parser;
+use image::io::Reader as ImageReader;
 use parser::Args;
 
 fn main() {
     let args = Args::parse();
+
+    let image = ImageReader::open(&args.file)
+        .expect("Failed to read in image")
+        .decode()
+        .expect("Failed to decode")
+        .to_rgb32f();
+
+    let (h, w) = image.dimensions();
+
+    println!("{}, {}", h, w);
+
+    for &pixel in image.pixels() {
+        println!("{:?}", pixel);
+    }
 
     println!("{}", args.file);
 }

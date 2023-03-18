be988756c4b6425acb0dc53db14a04c82562f91d
Ussama Azam <nuclearoreo@gmail.com>
2023-03-17 21:24:01 -0400

    Clean for change of plans

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,8 @@ mod parser;
 use clap::Parser;
 use parser::Args;
 
-//https://texteditor.com/ascii-art/
 fn main() {
     let args = Args::parse();
-    let output = b"".to_vec();
 
-    let test = r#"
-    ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-    ██░███░█▀▄▄▀█▀▄▄▀█░███░█░███░
-    ██░█░█░█░██░█░██░█▄▀░▀▄█▄▀░▀▄
-    ██▄▀▄▀▄██▄▄███▄▄███▄█▄███▄█▄█
-    ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"#;
-
-    let bytes = test.as_bytes();
-
-    let mut byte = vec![];
-    for (i, &u) in bytes.iter().enumerate() {
-        byte.push(u);
-        let char = String::from_utf8_lossy(&byte[..]);
-
-        println!("{}", char);
-
-        if i == 10 {
-            break;
-        }
-    }
-
-    let mut vec = bytes.to_vec();
-    vec.append(&mut bytes.to_vec());
-
-    match String::from_utf8(vec) {
-        Ok(result) => {
-            println!("{}", result);
-        }
-        Err(_) => panic!("Failed to output Art"),
-    };
+    println!("{}", args.file);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,18 +1,14 @@
 use clap::Parser;
 
-/// Pretty Text, CLI to convert your text or photo into ASCII Art
+/// Pretty Text, CLI to convert your photo into ASCII Art
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    /// Convert from text or photo, [options: text, photo]
-    #[arg(short, long, default_value = "text")]
-    pub medium: String,
-
-    /// Input text you want convert into ASCII
+    /// File path of the image
     #[arg(short, long)]
-    pub text: String,
+    pub file: String,
 
-    /// Font style of text
+    /// Style of ASCII Art
     #[arg(short, long, default_value = "Meh")]
-    pub font: String,
+    pub style: String,
 }

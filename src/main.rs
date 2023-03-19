82c37629d7cc5df6b1321528101e502c760af971
Ussama Azam <nuclearoreo@gmail.com>
2023-03-18 21:18:39 -0400

    Able to replace chars

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,29 @@ fn grab_image(args: &Args) -> DynamicImage {
 }
 
 fn resize_image(args: &Args, image: &DynamicImage) -> DynamicImage {
-    image.resize(10, 10, Gaussian)
+    image.resize(30, 30, Gaussian)
 }
 
 fn main() {
-    let _ASCII_CHAR = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.'];
+    let ascii_char = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.'];
     let args = Args::parse();
 
     let image = grab_image(&args);
     let resized_image = resize_image(&args, &image);
 
-    let _gray_scale = image.to_luma8();
+    let gray_scale = resized_image.to_luma8();
+
+    let (w, h) = gray_scale.dimensions();
+
+    let mut matrix = vec![vec!['n'; w as _]; h as _];
+
+    for (x, y, pixel) in gray_scale.enumerate_pixels() {
+        let (j, i) = (x as usize, y as usize);
+        let index = (pixel.0[0] / 25) as usize;
+        matrix[i][j] = ascii_char[index];
+    }
+
+    for row in matrix.iter() {
+        println!("{:?}", row);
+    }
 }
